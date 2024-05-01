aef5825a04aaa26ff62864293c5802d42002ed80
Joshua <joshuaugwuede@gmail.com>
2024-05-01 15:58:15 +0100

    good

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,10 +8,10 @@ import Footer2 from "../components/Footer2";
 
 const HomePage = () => {
   const address = "0xadkf............aslfgl";
-  const balance = "9,100,550,044";
+  const balance = "99,044";
 
   return (
-    <div className=" h-screen flex flex-col gap-4 justify-between bg-white">
+    <div className="h-svh flex flex-col gap-4 justify-between bg-white">
       <div className="m-3 mx-6 flex gap-4 flex-col h-full ">
         <HeaderComp />
 
